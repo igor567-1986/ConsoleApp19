84e177d7ca4d5102f983f6ad8f95b87d429a1ef9
Petro_qj70@top-academy.ru <Petro_qj70@top-academy.ru>
2023-01-27 21:41:12 +0300

    class_work_end

--- ConsoleApp19/Program.cs
+++ ConsoleApp19/Program.cs
@@ -1,6 +1,31 @@
 ﻿using static System.Console;
-WriteLine("Hello, World!");
-WriteLine("Привет мир");
+
+//object buf = 6;
+//object temp = 7.5;
+//WriteLine("Hello, World!");
+//WriteLine("Привет мир");
+//WriteLine($"Пример {buf} {temp}");
+//int count = 0;
+//for(byte i=0;i<10;i++)
+//    for(byte j=0;j<10;j++)
+//        for(byte k=0;k<10;k++)
+//            for(byte l=0;l<10;l++)
+//                for(byte m=0;m<10;m++)
+//                    for(byte n=0;n<10;n++)
+//                    {
+//                        if ((i + j + k) == 0)
+//                            continue;
+//                        if((i + j + k) == (l+m+n))
+//                            count++;
+                        
+//                    }
+//WriteLine(count);
+
+int a=Convert.ToInt32(Console.ReadLine());
+int[] nums =new int[a];//берет из консоли введеную переменную использует как размер массива
+// автоматически забивает нулями если не проинициализированно не чем
+int[] nums2 = new int[] { 1, 2, 3, 4 };// при такой записи автоматом выставляет размер массива
+// от колличества введеных значений
 
 
 
